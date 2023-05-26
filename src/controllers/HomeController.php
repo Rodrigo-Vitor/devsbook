b3d92ac653a67c5d6bd57e6a96cc20246d360106
Rodrigo-Vitor <rodrigovitor400@gmail.com>
2023-05-26 09:27:00 -0300

    Criando login

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,20 +1,21 @@
 <?php
 namespace src\controllers;
 
+use src\handlers\LoginHandler;
 use \core\Controller;
 
 class HomeController extends Controller {
+    private $loggedUser;
+
+    public function __construct() {
+        $this->loggedUser = LoginHandler::checkLogin();
+        if($this->loggedUser === false) {
+            $this->redirect('/login');
+        }
+    }
 
     public function index() {
         $this->render('home', ['nome' => 'Bonieky']);
-    }
-
-    public function sobre() {
-        $this->render('sobre');
-    }
-
-    public function sobreP($args) {
-        print_r($args);
     }
 
 }
